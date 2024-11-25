5feb03156ee89e43c9fe49b5abaf47c7fe829b28
diegofmsantos <diegofmsantos0@gmail.com>
2024-11-24 21:03:31 -0300

    change db

--- prisma/migrations/20241112211419_estatistica_e_opcional_agora/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Jogador" ALTER COLUMN "estatisticas" DROP NOT NULL;
--- prisma/migrations/20241119175122_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Jogador" ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "Jogador_id_seq";
-
--- AlterTable
-ALTER TABLE "Time" ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "Time_id_seq";
